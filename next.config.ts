da3e3d0e951d31a34716042b0651b9d7fb713a33
Tin Do <dothanhtin003@gmail.com>
2026-07-17 22:05:55 -0700

    Whitelist the path of images from stevenscreektoyota

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https", // Or 'http' if the site isn't SSL
+                hostname: "www.stevenscreektoyota.com", // Use the actual domain of the dealer
+                pathname: "/inventoryphotos/**",
+            },
+        ],
+        // If you prefer to handle the path locally as you were trying:
+        localPatterns: [
+            {
+                pathname: "/inventoryphotos/**",
+                search: "",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
